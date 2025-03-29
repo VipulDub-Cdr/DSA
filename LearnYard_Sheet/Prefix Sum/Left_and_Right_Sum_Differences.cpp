8d839579468a2d5ed82e11835087d5f23780d8a5
VipulDub-Cdr <vipuldubey041@gmail.com>
2025-03-29 12:47:01 +0530

    LRSUM

--- LearnYard_Sheet/Prefix Sum/Left_and_Right_Sum_Differences.cpp
+++ LearnYard_Sheet/Prefix Sum/Left_and_Right_Sum_Differences.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int lRSumDifference(){
-    
+vector<int> lRSumDifference(vector<int> &arr){
+    int n = arr.size();
+    vector<int> left(n), right(n);
+    left[0] = arr[0];
+    for(int i = 1; i < n; i++){
+        left[i] = left[i-1] + arr[i];
+    }
+    right[n-1] = arr[n-1];
+    for(int i = n-2; i >= 0; i--){
+        right[i] = right[i+1] + arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        left[i] = abs(left[i] - right[i]);
+    }
+    return arr;
 }
 int main() {
-    
+    vector<int> arr = {1, 2, 3, 4, 5};
+    vector<int> result = lRSumDifference(arr);
     return 0;
 }
