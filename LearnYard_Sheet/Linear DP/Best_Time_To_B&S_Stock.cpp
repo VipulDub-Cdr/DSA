eda5c17f6f7b7c1a609f62bcdfa1a7f90da134ba
VipulDub-Cdr <vipuldubey041@gmail.com>
2025-08-01 18:28:39 +0530

    stock Buy and sell DP

---
+++ LearnYard_Sheet/Linear DP/Best_Time_To_B&S_Stock.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& prices){
+    int n = prices.size();
+
+    int maxProfit = prices[0];
+    int minPrice = 0;
+    
+    for(int i=1;i<n;i++){
+        int price = prices[0];
+
+        minPrice = min(minPrice,price);
+
+        int currentProfit = price - minPrice;
+
+        maxProfit = max(maxProfit,currentProfit);
+    }
+    return maxProfit;
+}
+
+int main(){
+    return 0;
+}
